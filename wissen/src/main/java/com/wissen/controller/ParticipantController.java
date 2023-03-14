735f90aa19ca77f4fc8d86fcfb6796b445140d78
Shivangi4294 <shivangi.gupta@wissen.com>
2023-03-14 16:52:22 +0530

    api

--- wissen/src/main/java/com/wissen/controller/ParticipantController.java
+++ wissen/src/main/java/com/wissen/controller/ParticipantController.java
@@ -7,14 +7,15 @@ public class participantController {
     ParticipantRepository repository;
 
     @PostMapping(path = "api/bulkParticipants")
-    public ParticipantEntity<String> insertParticipants(){
-        Participant participant1 = new Participant("1","Akash","O9", "DS","Data Scientist");
-        Participant participant2 = new Participant("2","Asit","Merlin", "Law","Lawyer");
-        Participant participant3 = new Participant("3","Shivi","Wissen", "Dev","Java Dev");
-        Participant participant4 = new Participant("4","Sudha","Li", "Management","Manager");
-        Participant participant5 = new Participant("5","Aditi","Bank", "Finance","Data");
+    public ParticipantEntity<String> insertParticipants(@RequestBody Participant participants){
+//        Participant participant1 = new Participant("1","Akash","O9", "DS","Data Scientist");
+//        Participant participant2 = new Participant("2","Asit","Merlin", "Law","Lawyer");
+//        Participant participant3 = new Participant("3","Shivi","Wissen", "Dev","Java Dev");
+//        Participant participant4 = new Participant("4","Sudha","Li", "Management","Manager");
+//        Participant participant5 = new Participant("5","Aditi","Bank", "Finance","Data");
 
-        List<Participant> participants = Arrays.asList(participant1,participant2,participant3,participant4,participant5);
+//        List<Participant> participants = Arrays.asList(participant1,participant2,participant3,participant4,participant5);
+
         repository.saveAll(participants);
 
         return ParticipantEntity.created("/participants");
