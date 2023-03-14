bf844159f448235282716ca9391e57b2a7b8ab3c
Shivangi4294 <shivangi.gupta@wissen.com>
2023-03-14 16:55:37 +0530

    updated api

--- wissen/src/main/java/com/wissen/controller/ParticipantController.java
+++ wissen/src/main/java/com/wissen/controller/ParticipantController.java
@@ -1,35 +1,44 @@
 package com.wissen.controller;
 
+import com.wissen.entity.Participant;
+import com.wissen.repository.ParticipantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
 @RestController
-public class participantController {
+public class ParticipantController {
 
     @Autowired
     ParticipantRepository repository;
 
-    @PostMapping(path = "api/bulkParticipants")
-    public ParticipantEntity<String> insertParticipants(@RequestBody Participant participants){
-//        Participant participant1 = new Participant("1","Akash","O9", "DS","Data Scientist");
-//        Participant participant2 = new Participant("2","Asit","Merlin", "Law","Lawyer");
-//        Participant participant3 = new Participant("3","Shivi","Wissen", "Dev","Java Dev");
-//        Participant participant4 = new Participant("4","Sudha","Li", "Management","Manager");
-//        Participant participant5 = new Participant("5","Aditi","Bank", "Finance","Data");
+    @PostMapping(path = "/insertParticipant")
+    public ResponseEntity<String> insertParticipants(){
 
-//        List<Participant> participants = Arrays.asList(participant1,participant2,participant3,participant4,participant5);
+        Participant p1 = new Participant(1,"Hari","wissen","tech","dev");
+        repository.save(p1);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PostMapping(path = "api/bulkParticipants")
+//    public ResponseEntity<String> insertParticipants(@RequestBody List<Participant> participants){
+    public void insertParticipants(@RequestBody List<Participant> participants){
 
         repository.saveAll(participants);
 
-        return ParticipantEntity.created("/participants");
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping(path ="api/getAllParticipants")     //@RequestMapping(method = RequestMethod.GET)
-    public List<Participant> getAllParticipants(){
-        List<Participant> participantsList = repository.getAllParticipants;
-        return participantsList;
-    }
-    @GetMapping(path ="api/getParticipantById")     //@RequestMapping(method = RequestMethod.GET)
-    public List<Participant> getAllParticipants(){
-        List<Participant> participantsList = repository.getAllParticipants;
-        return participantsList;
-    }
+//    @GetMapping(path ="api/getAllParticipants")     //@RequestMapping(method = RequestMethod.GET)
+//    public List<Participant> getAllParticipants(){
+//        List<Participant> participantsList = repository.getAllParticipants;
+//        return participantsList;
+//    }
+//    @GetMapping(path ="api/getParticipantById")     //@RequestMapping(method = RequestMethod.GET)
+//    public List<Participant> getAllParticipants(){
+//        List<Participant> participantsList = repository.getAllParticipants;
+//        return participantsList;
+//    }
 
 }
---
+++ wissen/src/main/java/com/wissen/repository/ParticipantRepository.java
@@ -0,0 +1,8 @@
+package com.wissen.repository;
+
+import com.wissen.entity.Participant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantRepository extends JpaRepository<Participant,Integer>{
+
+}
