5af7acb1b55a4bfffc99b92b1d945170c898b8af
Shivangi4294 <shivangi.gupta@wissen.com>
2023-03-18 10:35:55 +0530

    updating service layer

--- wissen/src/main/java/com/wissen/service/ParticipantService.java
+++ wissen/src/main/java/com/wissen/service/ParticipantService.java
@@ -1,2 +1,15 @@
-package com.wissen.service;public interface ParticipantService {
+package com.wissen.service;
+
+import com.wissen.entity.Participant;
+import com.wissen.exception.IdNotFoundException;
+import com.wissen.exception.MandatoryFieldException;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
+public interface ParticipantService {
+    ResponseEntity<Participant> insertParticipant(Participant participant) throws MandatoryFieldException;
+    List<Participant> insertBulkParticipants(List<Participant> participants);
+    Participant getParticipantById(int id);
+    List<Participant> getAllParticipants();
+
 }
--- wissen/src/main/java/com/wissen/service/ParticipantServiceBeanImpl.java
+++ wissen/src/main/java/com/wissen/service/ParticipantServiceBeanImpl.java
@@ -1,2 +1,55 @@
-package com.wissen.service;public class ParticipantServiceBeanImpl {
+package com.wissen.service;
+
+import com.wissen.entity.Participant;
+import com.wissen.exception.MandatoryFieldException;
+import com.wissen.exception.ParticipantNotFoundException;
+import com.wissen.repository.ParticipantRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import org.slf4j.*;
+import com.wissen.exception.IdNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Service
+@Slf4j
+public  class ParticipantServiceBeanImpl implements ParticipantService {
+    Logger logger = LoggerFactory.getLogger(ParticipantServiceBeanImpl.class);
+    @Autowired
+    private ParticipantRepository repository;
+    @ExceptionHandler(value = MandatoryFieldException.class)
+    public ResponseEntity<Participant> insertParticipant(Participant participant) throws MandatoryFieldException {
+
+        if(participant.getParticipantName().isEmpty() || participant.getParticipantName().isBlank()) {
+            throw new MandatoryFieldException("ParticipantName is mandatory.");
+        }
+        return new ResponseEntity<>(repository.save(participant),HttpStatus.OK);
+    }
+    public List<Participant> insertBulkParticipants(List<Participant> participants){
+        return repository.saveAll(participants);
+    }
+    @ExceptionHandler(value = IdNotFoundException.class)
+    public Participant getParticipantById(int id) {
+        Participant participant;
+        Optional<Participant> optional = repository.findById(id);
+        if(optional.isPresent()){
+            participant  = optional.get();
+        }
+        else{
+            throw new IdNotFoundException("Participant not found.");
+        }
+        return participant;
+    }
+
+    @ExceptionHandler(value = ParticipantNotFoundException.class)
+    public List<Participant> getAllParticipants(){
+        List<Participant> participantList = repository.findAll();
+        if(participantList.isEmpty()){
+            throw new ParticipantNotFoundException("No Participant to show.");
+        }
+        return participantList;
+    }
 }
