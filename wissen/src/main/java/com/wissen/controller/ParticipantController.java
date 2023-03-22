95ced61b9162026dc6b670714c70e945b6d6dce4
Shivangi4294 <shivangi.gupta@wissen.com>
2023-03-22 17:04:25 +0530

    Postman api and ddl-create

--- wissen/src/main/java/com/wissen/controller/ParticipantController.java
+++ wissen/src/main/java/com/wissen/controller/ParticipantController.java
@@ -5,7 +5,6 @@ import com.wissen.service.ParticipantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
 
 @RestController
